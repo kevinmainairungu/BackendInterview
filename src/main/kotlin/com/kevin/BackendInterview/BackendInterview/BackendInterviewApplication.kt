0b86e9226d106c4881ecbbcce6e4b37a15cf4363
kevinmainairungu <mainairungu99@gmail.com>
2021-05-12 13:43:28 +0300

    springboot project init

---
+++ src/main/kotlin/com/kevin/BackendInterview/BackendInterview/BackendInterviewApplication.kt
@@ -0,0 +1,11 @@
+package com.kevin.BackendInterview.BackendInterview
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BackendInterviewApplication
+
+fun main(args: Array<String>) {
+	runApplication<BackendInterviewApplication>(*args)
+}
---
+++ src/test/kotlin/com/kevin/BackendInterview/BackendInterview/BackendInterviewApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.kevin.BackendInterview.BackendInterview
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BackendInterviewApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
